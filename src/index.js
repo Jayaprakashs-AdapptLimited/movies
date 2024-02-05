696bd8c2642c6a0733baf3d8acaba3e23365364a
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-02-05 19:32:04 +0530

    Added SratRating comp, fetch data from api using useEffect

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import StarRating from './StarRating';
+// import App from './App-TextExpander'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <App /> */}
+    {/* <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Ok', 'Good', 'Amazing']} /> */}
+
+    {/* <StarRating maxRating={5} /> */}
   </React.StrictMode>
 );
 
